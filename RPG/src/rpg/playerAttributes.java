c2cea05dcef9e4be89f63e2d25a173e63d0980bc
Raf <69535896+Rafee-M@users.noreply.github.com>
2024-03-28 19:53:02 +0000

    main class (empty) and character class

---
+++ RPG/src/rpg/playerAttributes.java
@@ -0,0 +1,79 @@
+package rpg;
+
+/**
+ *
+ * @author Rafee-M
+ */
+public class playerAttributes {
+    
+    //variables
+    
+    //player direct stats
+    private String name;
+    private int maxHealth = 50;
+    private int health = 50;
+    private int maxDamage = 10;
+    
+    //inventory
+    private int healthPotion = 3;
+    private int healthPotionHeal = 20;
+    
+    //player attributes
+    private int abilityPoints = 10;
+    private int strength = 0;
+    private int stamina = 0;
+    private int intelligence = 0;
+    private int charisma = 0;
+    
+    //empty constructor
+    public playerAttributes(){
+        
+    }
+    //setters direct stats
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setMaxHealth(int maxHealth){
+        this.maxHealth = maxHealth;
+    }
+    public void setHealth(int health){
+        this.health = health;
+    } 
+    public void setMaxDamage(int maxDamage){
+        this.maxDamage = maxDamage;
+    }
+    //inventory
+    public void setHealthPotion(){
+        this.healthPotion++;
+    }
+    
+    //setters - player attributes
+    public void setStrength(){
+        if(this.abilityPoints > 0){
+            this.strength++;
+            this.abilityPoints --;
+        }
+    }
+    public void setCharisma(){
+        if(this.abilityPoints > 0){
+            this.charisma++;
+            this.abilityPoints --;
+        }
+    }
+    public void setStamina(){
+        if(this.abilityPoints > 0){
+            this.stamina++;
+            this.abilityPoints --;
+        }
+    }
+    public void setIntelligence(){
+        if(this.abilityPoints > 0){
+            this.intelligence++;
+            this.abilityPoints --;
+        }
+    }
+    
+    //setters
+    
+    
+}
