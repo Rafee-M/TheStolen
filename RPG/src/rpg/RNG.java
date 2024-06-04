287dccf25cfe728eecff104de797a2cb78884fda
Raf <69535896+Rafee-M@users.noreply.github.com>
2024-06-04 16:22:02 +0000

    Update RNG.java

--- RPG/src/rpg/RNG.java
+++ RPG/src/rpg/RNG.java
@@ -1,24 +1,19 @@
 package rpg;
+
 /**
  *
  * @author Rafee-M
  */
+
 import java.util.Random;
 
 public class RNG {
     // Method to generate random damage
-    public int generateRandomDamage(playerAttributes player) {
+    
+    public int generate(){
+        
         Random rand = new Random();
-        // Random damage between 1 and maxDamage value from playerAttributes object
-        int damage = rand.nextInt(player.getMaxDamage()) + 1;
-        return damage;
+        return rand.nextInt(21);
     }
 
-    // Method to generate random healing by potion
-    public int generateRandomHealing(playerAttributes player) {
-        Random rand = new Random();
-        // Random healing between 1 and healthPotionHeal value from playerAttributes object
-        int healing = rand.nextInt(player.getHealthPotionHeal()) + 1;
-        return healing;
-    }
 }
